215b80c874ce64debd9e15a705e02bfb95d96b38
komalarao <manoharvakamullu1999@gmail.com>
2023-02-21 18:36:06 -0800

    added: welcome message

---
+++ src/addressBookServiceDB/WelcomeMessage.java
@@ -0,0 +1,9 @@
+package addressBookServiceDB;
+
+public class WelcomeMessage {
+
+	public static void main (String args[]) {
+		
+		System.out.print(" welcome to addressBook service program on sql:");
+	}
+}
